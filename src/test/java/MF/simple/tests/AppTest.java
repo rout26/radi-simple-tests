5dcf0e3b8d0d41d25f95bdae835fa12d4049ce30
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-07-07 22:37:06 +0300

    send get

--- src/test/java/MF/simple/tests/AppTest.java
+++ src/test/java/MF/simple/tests/AppTest.java
@@ -7,49 +7,49 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testAppA()
-    {
-        assertTrue( true );
+    public void testAppA() {
+        assertTrue(true);
     }
 
-    public void testAppB()
-    {
-        assertTrue( true );
+    public void testAppB() {
+        assertTrue(true);
     }
 
-    public void testAppC()
-    {
-        assertTrue( true );
+    public void testAppC() {
+        assertTrue(true);
     }
 
-    public void testWillFailOnFailParameter()
-    {
+    public void testWillFailOnFailParameter() {
         String value = System.getProperty("fail");
-        if(value!=null && value.toLowerCase().equals("true")){
+        if (value != null && value.toLowerCase().equals("true")) {
             fail("fail parameter is set");
+        }
+    }
+
+    public void testWillThrowOnThrowParameter() {
+        String value = System.getProperty("throw");
+        if (value != null && value.toLowerCase().equals("true")) {
+            throw new RuntimeException("Throw parameter is set");
         }
     }
 }
