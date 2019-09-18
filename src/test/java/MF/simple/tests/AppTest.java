ced565561dc5714b3abd96d1b2b4108aeffa17c7
Radislav Berkovich <radislav77@gmail.com>
2019-09-18 11:30:25 +0300

    Update AppTest.java

--- src/test/java/MF/simple/tests/AppTest.java
+++ src/test/java/MF/simple/tests/AppTest.java
@@ -40,6 +40,10 @@ public class AppTest extends TestCase {
     public void testAppC() {
         assertTrue(true);
     }
+    
+    public void testLongName 1_0123456789 2_0123456789 3_0123456789 4_0123456789 5_0123456789 6_0123456789 7_0123456789 8_0123456789 9_0123456789 10_0123456789 11_0123456789 12_0123456789 13_0123456789 14_0123456789 15_0123456789 16_0123456789 17_0123456789 18_0123456789 19_0123456789 20_0123456789() {
+        assertTrue(true);
+    }
 
     public void testWillFailOnFailParameter() {
         String value = System.getProperty("fail");
